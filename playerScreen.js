529cd690a4c36c5cfb640bd30986c04fc54bc1d7
opalwb <opalwb@gmail.com>
2022-02-10 22:20:55 -0600

    Added stylesheet. Fixed letter input. Added multiple guesses.

--- playerScreen.js
+++ playerScreen.js
@@ -5,6 +5,7 @@ let boardHolder = document.getElementById("GameHolder");
 let currentGuess = 0;
 let nextLetter = 0;
 
+let gameRows = [];
 
 function setupGame(){
     makeEmptyBoard();
@@ -21,11 +22,13 @@ function makeEmptyBoard(){
     for(rowNumber = 0; rowNumber < numGuesses; rowNumber++){
         gameRow = document.createElement("div");
         gameRow.id = `Row${rowNumber}`;
+        gameRow.classList.add("gameRow")
+        gameRows.push(gameRow);
         boardHolder.appendChild(gameRow);
         for (letterNumber = 0; letterNumber < wordLength; letterNumber++){
             letterTile = document.createElement("div");
             letterTile.id = `Row${rowNumber}letter${letterNumber}`;
-            letterTile.class = "emptyLetter";
+            letterTile.classList.add("gameTile");
             gameRow.appendChild(letterTile);
         }
     }
@@ -34,38 +37,64 @@ function makeEmptyBoard(){
 
 function onLetterTyped(e){
 
+    let guessRow = gameRows[currentGuess];
+    console.log(guessRow);
+
     //make sure it's a valid input. Later.
     input = e.key;
-    console.log(input);
-
-    //find the right box to change
-    
-    let guessRowID = `Row${currentGuess}`;
-    let guessRow = document.getElementById(guessRowID);
-    if ( nextLetter < wordLength){
-        nextNode = guessRow.childNodes[nextLetter];
-    }
-
-
-    if(input == "Backspace"){
-        if(nextLetter >= 0){
-            //remove a letter
-            nextNode.innerHTML = ""; //probably a better way to clear it out but we are doing science
-            //go back
-            nextLetter--;
+    if(input == "Enter"){
+        let guess = getGuess(guessRow);
+        if(validGuess(guess)){
+            nextGuess(guess);
         }
     }
-    //assume nobody's gonna type a number because i'm lazy right now
-    else{
-        console.log("valid")
-        nextNode.innerHTML = input;
-        nextLetter++;
-    } 
+        //find the right box to change
 
-    //if allowed, put the letter in the box or remove a letter from the box
+    //if it's a backspace, it's the last box that changed. If it's a letter, it's the next box.
+    else if(input == "Backspace"){
+        if(nextLetter > 0){
+            nextLetter--;
+            nextNode = guessRow.childNodes[nextLetter];
+            //remove a letter
+            nextNode.innerHTML = ""; //probably a better way to clear it out but we are doing science
+        }
+    }
+    else {
+        if ( nextLetter < wordLength){
+            nextNode = guessRow.childNodes[nextLetter];
+            nextNode.innerHTML = input;
+            nextLetter++;
+        }
+    }
+    
 
     //update the next letter counter
 }
+
+//takes a guess row and spits out a string of the guess.
+function getGuess(guessRow){
+    let guess = "";
+    for(child of guessRow.children){
+        guess = guess.concat(child.textContent);
+    }
+    return guess;
+}
+
+function validGuess(guess){
+    //this function checks to see if the guess is a word.
+    //That sounds like work.
+    //Also it's not needed to test our logic!
+    return (guess.length == wordLength) ? true : false;
+    
+}
+
+function nextGuess(){
+    let guessRow = gameRows[currentGuess];
+    guessRow.classList.add("completedGuess");
+    currentGuess++;
+    nextLetter = 0;
+}
+
 
 setupGame();
 
